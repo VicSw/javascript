4538b9dbefefb7e1954a9e7b221997532757cfab
vicSun <sunwei@192.168.0.106>
2017-06-15 00:18:57 +0800

    Animation
    Share

--- Study/Animation/app.js
+++ Study/Animation/app.js
@@ -1,3 +1,41 @@
 /**
  * Created by sunwei on 2017/6/14.
  */
+window.onload=function () {
+    var vDiv=document.getElementById("divMenu");
+
+    vDiv.onmouseover=function () {
+        StartMove();
+    };
+
+    vDiv.onmouseout=function () {
+        EndMove();
+    }
+}
+
+var timer=null;
+function StartMove() {
+    clearInterval(timer);
+    var vDiv=document.getElementById("divMenu");
+        timer=setInterval(function () {
+           if(vDiv.offsetLeft==0){
+               clearInterval(timer);
+           }else {
+               vDiv.style.left=vDiv.offsetLeft+10+'px';
+           }
+        },30)
+
+}
+function EndMove() {
+    clearInterval(timer);
+    var vDiv=document.getElementById("divMenu");
+    timer=setInterval(function () {
+        if(vDiv.offsetLeft==-200){
+            clearInterval(timer);
+        }else {
+            vDiv.style.left=vDiv.offsetLeft-10+'px';
+        }
+    },30)
+
+}
+
