724c6872bdfe53f9dbb0ad86800851efbc79e0d6
sw <sw@sw.com>
2017-06-19 17:23:21 +0800

    Create Alpha

---
+++ Study/Animation/Alpha/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sw on 2017/6/19.
+ */
+window.onload(function () {
+    var vDiv=document.getElementById("alphaDiv");
+    vDiv.onmouseover=function () {
+
+    }
+});
+
+var timer=null;
+function StartMove() {
+    clearInterval(timer);
+    timer=setInterval(function () {
+
+    },30)
+}
