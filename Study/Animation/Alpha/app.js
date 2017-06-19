f57994a07b99c7c920b22ff0fff07244e7a5c7e0
sw <sw@sw.com>
2017-06-19 17:41:06 +0800

    finish

--- Study/Animation/Alpha/app.js
+++ Study/Animation/Alpha/app.js
@@ -1,17 +1,35 @@
 /**
  * Created by sw on 2017/6/19.
  */
-window.onload(function () {
+window.onload=function () {
     var vDiv=document.getElementById("alphaDiv");
     vDiv.onmouseover=function () {
-
+        StartMove(30);
     }
-});
+
+    vDiv.onmouseout=function () {
+        StartMove(100);
+    }
+};
 
 var timer=null;
-function StartMove() {
+var alpha=30;
+function StartMove(temp) {
+    var vDiv=document.getElementById("alphaDiv");
     clearInterval(timer);
     timer=setInterval(function () {
-
+        var speed=0;
+        if(alpha>temp){
+            speed=-10;
+        }else {
+            speed=10;
+        }
+        if(alpha==temp){
+            clearInterval(timer);
+        }else {
+            alpha+=speed;
+            vDiv.style.filter='alpha(opacity:'+alpha+')';
+            vDiv.style.opacity=alpha/100;
+        }
     },30)
 }
